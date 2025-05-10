9d5323e3f96648db88ef3547d6bf4c3c4ab597bc
woollim <137277221+woollim23@users.noreply.github.com>
2025-05-10 17:21:59 +0900

    update: 매크로 로그 ABLOG 선언 (3챕터 시작)

--- ArenaBattle/Source/ArenaBattle/Private/Fountain.cpp
+++ ArenaBattle/Source/ArenaBattle/Private/Fountain.cpp
@@ -54,6 +54,8 @@ void AFountain::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	ABLOG_S(Warning);
+	ABLOG(Warning, TEXT("Actor Name : %s, ID : %d, Location X : %.3f"), *GetName(), ID, GetActorLocation().X);
 }
 
 // Called every frame
